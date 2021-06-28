9a400f5d688056ef634d9c91e22ce38eaa2027f4
James Rosado <jamesros@boldgrid.com>
2021-06-28 14:43:12 -0400

    disable gutenberg widget editor

--- functions.php
+++ functions.php
@@ -4,6 +4,16 @@
  *
  * @package Prime
  */
+add_action( 'after_theme_setup', 'bgtfw_block_widgets' );
+
+/**
+ * Bgtfw Block Widgets.
+ *
+ * Disable block widget editor.
+ */
+function bgtfw_block_widgets() {
+	remove_theme_support( 'widgets-block-editor' );
+}
 
 locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
 require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
