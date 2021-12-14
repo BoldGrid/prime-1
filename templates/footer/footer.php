f613c4b855241292891ae3d234ed1c0f0c28442b
James Rosado <jamesros@boldgrid.com>
2021-12-14 17:32:27 -0500

    add template footer classes

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -7,10 +7,20 @@
  * @since 2.0
  * @package Prime
  */
+$has_footer_template     = apply_filters( 'crio_premium_get_page_footer', get_the_ID() );
+$has_footer_template     = get_the_ID() === $has_footer_template ? false : $has_footer_template;
+$footer_content_classes  = array( 'bgtfw-footer', 'footer-content' );
+$footer_colophon_classes = array( 'site-footer' );
+
+if ( $has_footer_template ) {
+	$footer_content_classes[]  = 'template-footer';
+	$footer_colophon_classes[] = 'template-footer';
+}
+
 ?>
-<footer id="colophon" <?php BoldGrid::add_class( 'footer', [ 'site-footer' ] ); ?> role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
+<footer id="colophon" <?php BoldGrid::add_class( 'footer', $footer_colophon_classes ); ?> role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
 	<?php do_action( 'boldgrid_footer_top' ); ?>
-	<div <?php BoldGrid::add_class( 'footer_content', [ 'bgtfw-footer', 'footer-content' ] ); ?>>
+	<div <?php BoldGrid::add_class( 'footer_content', $footer_content_classes ); ?>>
 		<?php echo BoldGrid::dynamic_footer(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 	</div>
 	<?php do_action( 'boldgrid_footer_bottom' ); ?>
